f2e2fbbd86efdd98a24d9d6573233993bbbd3111
TKForgeron <trollololollolol.js@gmail.com>
2021-10-29 11:40:28 +0200

    finishing measures.py

--- measures.py
+++ measures.py
@@ -1,19 +1,7 @@
 import pandas as pd
 import json
 import requests
-from datetime import datetime
-
-
-def get_measures_df_il() -> pd.DataFrame:
-    pass
-
-
-def get_measures_df_nl() -> pd.DataFrame:
-    pass
-
-
-def get_measures_df_nsw() -> pd.DataFrame:
-    pass
+from datetime import datetime, timedelta
 
 
 def get_measures_df(start_date: datetime = None) -> pd.DataFrame:
@@ -24,9 +12,44 @@ def get_measures_df(start_date: datetime = None) -> pd.DataFrame:
     except:
         df = pd.read_csv("data/OxCGRT_latest.csv")
 
-    df = df[df["CountryCode"].isin(["AUS", "ISR", "NLD"])]
+    df.rename(
+        inplace=True,
+        columns={
+            "Date": "date",
+            "ConfirmedCases": "cases",
+            "ConfirmedDeaths": "deaths",
+        },
+    )
+    df["date"] = df["date"].astype(str)
+    df["date"] = df["date"].apply(lambda x: datetime.strptime(x, "%Y%m%d"))
+
+    if start_date:
+        df = df[df["date"] >= start_date]
 
     return df
 
 
-get_measures_df()
+def get_measures_df_il_nl_nsw(start_date: datetime = None) -> pd.DataFrame:
+
+    try:
+        url = "https://raw.githubusercontent.com/OxCGRT/covid-policy-tracker/master/data/OxCGRT_latest.csv"
+        df = pd.read_csv(url)
+    except:
+        df = pd.read_csv("data/OxCGRT_latest.csv")
+
+    df = df[df["CountryCode"].isin(["AUS", "ISR", "NLD"])]
+    df.rename(
+        inplace=True,
+        columns={
+            "Date": "date",
+            "ConfirmedCases": "cases",
+            "ConfirmedDeaths": "deaths",
+        },
+    )
+    df["date"] = df["date"].astype(str)
+    df["date"] = df["date"].apply(lambda x: datetime.strptime(x, "%Y%m%d"))
+
+    if start_date:
+        df = df[df["date"] >= start_date]
+
+    return df
---
+++ predictions.py
@@ -0,0 +1,8 @@
+from datetime import datetime
+import pandas as pd
+
+import measures as m
+import cases as c
+import hospitalizations as h
+
+df = m.get_measures_df()
