5361dbbf690e2805058040d74a10fe16f46213fc
Matthijs Blaauw <matthijsblaauw@Matthijss-MacBook-Pro-3.local>
2021-11-02 16:38:12 +0100

    better data_importer

--- data_importer.py
+++ data_importer.py
@@ -2,6 +2,7 @@ import cases
 import vaccinations
 import hospitalizations
 import measures
+import temperature
 import sys
 import time
 import os
@@ -13,6 +14,8 @@ class Data_Importer:
     def __init__(self, no_cache:bool=False):
         self.data = {}
         self.overwrite_cache = no_cache
+        self.progress = 0
+        self.max_progress = 14
     
     def get_data(self):
         loaded_from_cache = self.check_cache()
@@ -23,30 +26,37 @@ class Data_Importer:
                 print('Loaded data succesfully from cache')
                 return self.data
             print('Unable to load data from cache, downloading new!')
-        self.update_progress(0)
+        self.update_progress()
         self.data['cases_nl'] = cases.get_cases_df_nl()
-        self.update_progress(10)
+        self.update_progress()
         self.data['cases_il'] = cases.get_cases_df_il()
-        self.update_progress(20)
+        self.update_progress()
         self.data['cases_nsw'] = cases.get_cases_df_nsw()
 
-        self.update_progress(30)
+        self.update_progress()
         self.data['vaccinations_nl'] = vaccinations.get_vaccinations_df_nl()
-        self.update_progress(40)
+        self.update_progress()
+
         self.data['vaccinations_il'] = vaccinations.get_vaccinations_df_il()
-        self.update_progress(50)
+        self.update_progress()
         self.data['vaccinations_nsw'] = vaccinations.get_vaccinations_df_nsw()
 
-        self.update_progress(60)
+        self.update_progress()
         self.data['hospitalizations_nl'] = hospitalizations.get_hospitalizations_df_nl()
-        self.update_progress(70)
+        self.update_progress()
         self.data['hospitalizations_il'] = hospitalizations.get_hospitalizations_df_il()
-        self.update_progress(80)
+        self.update_progress()
         self.data['hospitalizations_nsw'] = hospitalizations.get_hospitalizations_df_nsw()
 
-        self.update_progress(90)
+        self.update_progress()
         self.data['measures'] = measures.get_measures_df_il_nl_nsw()
-        self.update_progress(100)
+        self.update_progress()
+        self.data['temperature_nl'] = temperature.get_temperatures_df_nl()
+        self.update_progress()
+        self.data['temperature_nsw'] = temperature.get_temperatures_df_nsw()
+        self.update_progress()
+        self.data['temperature_il'] = temperature.get_temperatures_df_il()
+        self.update_progress()
 
         self.write_cache()
         self.overwrite_cache = False
@@ -88,8 +98,9 @@ class Data_Importer:
             os.remove('./cache/' + f)
         os.rmdir('./cache')
 
-    def update_progress(self, progress):
-        finishedPart = int(progress/10)
-        if (progress == 100): 
-            sys.stdout.write('\r Importing / fetching website data [Done!]                            ')
-        sys.stdout.write('\r Importing / fetching website data [{0}] {1}%'.format(('#'*(finishedPart)) + (' '*(10-finishedPart)), progress))
+    def update_progress(self):
+        self.progress += 1
+        finishedPart = int(self.progress / self.max_progress * 100 / 2)
+        if (self.progress == 100): 
+            sys.stdout.write('\r Importing / fetching website data [Done!]                                ')
+        sys.stdout.write('\r Importing / fetching website data [{0}] {1}%'.format(('#'*int(finishedPart)) + (' '*(50-finishedPart)), finishedPart * 2))
